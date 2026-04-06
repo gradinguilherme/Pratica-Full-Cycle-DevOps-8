9fbcfca3d9ae11f3803d535ce848687baad1ff78
Guilherme Gradin <114577430+gradinguilherme@users.noreply.github.com>
2026-04-06 22:25:23 +0000

    Initial Commit

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "file_name" {
+  # definir um nome "default" para o arquivo a ser criado.
+  default = "example.txt"
+}
+
+variable "file_content" {
+  # definir um conteúdo "default" para o arquivo a ser criado.
+  default = "Here we will have the content for the text file"
+}
