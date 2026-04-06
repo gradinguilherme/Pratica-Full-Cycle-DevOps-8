586a441f4475429f1d502fd29c33449f2bd3febd
Guilherme Gradin <114577430+gradinguilherme@users.noreply.github.com>
2026-04-06 22:37:10 +0000

    feat: Config Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "local_file" "example" {
   # definir um "filename", apontando para a variável definida em "variables.tf"
   # definir um "content", apontando para a variável definida em "variables.tf"
-  file_name = var.file_name
+  filename = var.file_name
   content = var.file_content
 }
