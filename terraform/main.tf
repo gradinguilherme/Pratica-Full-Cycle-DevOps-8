e5a57df7e8cbbd3087bda7954d2cbe94a9579dbc
Guilherme Gradin <114577430+gradinguilherme@users.noreply.github.com>
2026-04-06 22:53:20 +0000

    fix: Terraform plan

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,4 @@
 resource "local_file" "example" {
-  # definir um "filename", apontando para a variável definida em "variables.tf"
-  # definir um "content", apontando para a variável definida em "variables.tf"
   filename = var.file_name
   content = var.file_content
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,7 @@
 variable "file_name" {
-  # definir um nome "default" para o arquivo a ser criado.
   default = "example.txt"
 }
 
 variable "file_content" {
-  # definir um conteúdo "default" para o arquivo a ser criado.
   default = "Here we will have the content for the text file"
 }
